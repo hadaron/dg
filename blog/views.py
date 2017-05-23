ac7e6cc2f8223ae2689defd4a394366a0275c61b
hadaron <hadaron@outlook.com>
2017-05-23 12:15:54 +0300

    bug fixed

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.utils import timezone
 from .models import Post
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render_to_response(request, 'blog/post_list.html', {'posts': posts})
